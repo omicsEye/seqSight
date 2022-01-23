1607b2c3e7bb2140d0a5775d052a804f1408d0fd
kellyzhang <kathyzhang415@gmail.com>
2022-01-23 12:00:11 -0500

    Modify path and deepStrain.py

---
+++ deepStrain/deepStrain.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import re
+import logging
+import shutil
+
+from deepStrain import config
+from deepStrain.utilities import utilities
+
+# def download_refDB():
+    # CMD
+
+def make_alignmnet_input():
+    a = 1
+
+def get_args():
+    b = 1
+
+def main():
+    args = get_args()
+
+    # Steps1: Make a directory or outputs
+    utilities.make_directory(config.output_folder)
+
+
+
+
+if __name__== '__main__':
+
